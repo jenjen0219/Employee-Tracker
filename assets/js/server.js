fde010dc06c331b01eb3a3d3a6963c13f2a37858
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-02-08 17:51:35 -0800

    added in the select case statement needed to help me toggle between questions based on user selection of main menu items along with the functions that will fire off based off those same selections

--- assets/js/server.js
+++ assets/js/server.js
@@ -10,6 +10,94 @@ const createConnection = () => {
     });
 };
 
+//the main menu function will be called first and will prompt the user to choose what they would like to do
+const mainMenu(){
+    return inquirer.prompt({
+        type: 'input',
+        name: 'initialize',
+        message: 'What would you like to do?',
+        choices: ['Add Department', 'Add Role', 'Add Employee', 'View Departments', 'View Roles', 'View Employees', 'Update Employee Role', 'Exit']
+    })
+}
+
+//the following select case will be used to determine which function to call based on the user's choice
+mainMenu().then(answers => {
+    switch (answers) {
+        case 'Add Department':
+            addDepartment();
+            break;
+        case 'Add Role':
+            addRole();
+            break;
+        case 'Add Employee':
+            addEmployee();
+            break;
+        case 'Update Employee Role':
+            updateEmployeeRole();
+            break;
+
+    }
+});
+
+function addDepartment() {
+    return inquirer.prompt({
+        type: 'input',
+        name: 'adding a department',
+        message: 'What will this department be called?',
+    })
+}
+
+function addRole() {
+    return inquirer.prompt({
+        type: 'input',
+        name: 'adding a role',
+        message: 'What will this role be called?',
+    },
+        {
+            type: 'input',
+            name: 'adding a role salary',
+            message: 'What is the salary for this role?',
+        },
+        {
+            type: 'input',
+            name: 'adding a role department',
+            message: 'What department will this role be in?',
+        })
+}
+function addEmployee() {
+    return inquirer.prompt({
+        type: 'input',
+        name: 'adding an employee first name',
+        message: 'What is the employee’s first name?',
+    }, {
+        type: 'input',
+        name: 'adding an employee last name',
+        message: 'What is the employee’s last name?',
+    },
+        {
+            type: 'input',
+            name: 'adding employee role',
+            message: 'What is the employee’s role?',
+        }, {
+        type: 'input',
+        name: 'adding employee manager',
+        message: 'Who is the employee’s manager?',
+    })
+}
+
+function updateEmployeeRole() {
+    return inquirer.prompt({
+        type: 'input',
+        name: 'employee selection for new role',
+        message: 'Which employee would you like to update?',
+    }, {
+        type: 'input',
+        name: 'updating employee role',
+        message: 'What department will this role be in?',
+        choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer'],
+
+    })
+}
 
 inquirer
     .prompt([
@@ -76,3 +164,5 @@ inquirer
     //         err ? console.log(err) : console.log('Successfully created index.html!')
     //     );
     // });
+
+    // create seperate functions for the various prompts
